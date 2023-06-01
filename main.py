5094052355347f0e3e8c74afcd050b0137a65789
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-06-01 18:44:16 +0530

    now nemesis-pkg can sync databases and determine it
    also add support for printing versions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from sys import argv, exit 
 from requests import get
 from subprocess import check_output 
+from os.path import isfile 
+from shutil import copy
+from requests.exceptions import MissingSchema, ConnectionError
 
 mainpage = """
 nemesis-pkg:
@@ -15,13 +18,49 @@ lri: lists packages in repository... nemesis-pkg lri {pkg}
 ug: upgrade packages... nemesis-pkg ug
 ud: update the package database... nemesis-pkg ud
 h: shows info on operations and usage.. nemesis-pkg h
+v: show nemesis-pkg version
 """
-operations = ["i" , "r" , "lsi" , "lri" , "ug" , "ud" , "h"]
+operations = ["i" , "r" , "lsi" , "lri" , "ug" , "ud" , "h" , "v"]
 path_pkglist = "/etc/nemesis-pkg/PKGLIST"
 path_ipkglist = "/etc/nemesis-pkg/IPKGLIST"
 current_user = check_output(["whoami"])
 pkglist_src = "https://raw.githubusercontent.com/Nemesis-OS/packages/main/PKGLIST"
 
+def update_database():
+    if isfile(path_pkglist) == True:
+        local_PKGLIST = True
+        pass
+    else:
+        local_PKGLIST = False
+        print("warning: database file is not found on path so downloading it")
+        pass
+
+    print("info: downloading database..")
+    try:
+        new_PKGLIST = get(pkglist_src)
+    except ConnectionError:
+        print("error: file failed to download due to connectivity issue")
+    except MissingSchema:
+        print("error: file failed to download due to some exceptions")
+
+    if new_PKGLIST != None and local_PKGLIST == True:
+        print("info: creating PKGLIST..")
+        local_PKGLIST = open(path_pkglist, 'w')
+        local_PKGLIST.write(str(new_PKGLIST.content.decode("utf-8")))
+        local_PKGLIST.close()
+    else:
+        local_PKGLIST = open(path_pkglist, 'r+')
+        if local_PKGLIST.read() != str(new_PKGLIST.content.decode("utf-8")):
+            print("info: replacing PKGLIST with the new one")
+            local_PKGLIST.seek(0)
+            local_PKGLIST.write(str(new_PKGLIST.content.decode("utf-8")))
+            local_PKGLIST.truncate()
+            pass
+        else:
+            print("info: you had the latest database")
+
+    print("sucess:- the databases were updated succesfully")
+        
 if __name__ == "__main__":
     if current_user != b'root\n':
         print("error: user is not root")
@@ -32,6 +71,10 @@ if __name__ == "__main__":
     try:
         if argv[1] == "h":
             print(mainpage)
+        elif argv[1] == "v":
+            print("nemesis-pkg 0.1(Build 2361)")
+        elif argv[1] == "ud":
+            update_database()
         else:
             print("error: invalid operation")
     except IndexError:
