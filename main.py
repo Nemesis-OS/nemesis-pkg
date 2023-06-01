19f97e1bd12b7c9eeeec3e22cceaf1a0390da8ed
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-06-01 15:20:31 +0530

    check if user is root..

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from sys import argv
+from sys import argv, exit 
+from requests import get
+from subprocess import check_output 
 
 mainpage = """
 nemesis-pkg:
@@ -16,9 +18,17 @@ h: shows info on operations and usage.. nemesis-pkg h
 """
 operations = ["i" , "r" , "lsi" , "lri" , "ug" , "ud" , "h"]
 path_pkglist = "/etc/nemesis-pkg/PKGLIST"
-path_ipkglist = "/e"
+path_ipkglist = "/etc/nemesis-pkg/IPKGLIST"
+current_user = check_output(["whoami"])
+pkglist_src = "https://raw.githubusercontent.com/Nemesis-OS/packages/main/PKGLIST"
 
 if __name__ == "__main__":
+    if current_user != b'root\n':
+        print("error: user is not root")
+        exit(1)
+    else:
+        pass
+    
     try:
         if argv[1] == "h":
             print(mainpage)
