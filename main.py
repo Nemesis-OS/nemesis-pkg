2ff83db2e10acacc29c80b46b8b25edd32f4f02c
Abhiraj Rik <BHK8624@dpsjaipur.com>
2023-06-16 14:27:32 +0530

    main.py
    rewrite nemesis-pkg
    
    added config parsing

--- main.py
+++ main.py
@@ -1,203 +1,38 @@
-from sys import argv, exit 
-from requests import get
-from subprocess import check_output 
-from os.path import isfile, isdir
 from os import mkdir, chdir
-from shutil import copy
-from requests.exceptions import MissingSchema, ConnectionError
+from os.path import isdir, isfile
+from sys import argv, exit, path
+from subprocess import check_output
+try:
+    from requests import get
+    from requests.exceptions import MissingSchema, ConnectionError
+except ImportError:
+    print("error: requests not found..")
+    exit(1)
 
-ANSI_CODES = [
-    "\x1b[31m",
-    "\x1b[32m",
-    "\x1b[33m",
-    "\x1b[34m",
-    "\x1b[0m"
-]
+disable_check_important_files = True
+ANSI_CODES = []
 
-mainpage = """nemesis-pkg:
-============
-usage:- nemesis-pkg {operation} {args}
-============
-operations:-
-i: installs a package.. nemesis-pkg i {pkg1} {pkg2}
-r: removes a package... nemesis-pkg r {pkg1} {pkg2}
-lsi: lists system installed packages.. nemesis-pkg lsi {pkg}
-lri: lists packages in repository... nemesis-pkg lri {pkg}
-ug: upgrade packages... nemesis-pkg ug
-ud: update the package database... nemesis-pkg ud
-h: shows info on operations and usage.. nemesis-pkg h
-v: show nemesis-pkg version"""
-
-operations = ["i" , "r" , "lsi" , "lri" , "ug" , "ud" , "h" , "v"]
-path_pkglist = "/etc/nemesis-pkg/PKGLIST"
-path_ipkglist = "/etc/nemesis-pkg/IPKGLIST"
-current_user = check_output(["whoami"])
-pkglist_src = "https://raw.githubusercontent.com/Nemesis-OS/packages/main/PKGLIST"
-cmd_args = []
-
-def update_database():
-    if isfile(path_pkglist) == True:
-        local_PKGLIST = True
+def parse_config_file():
+    global disable_check_important_files, ANSI_CODES
+    if isfile("/etc/nemesis-pkg/config.py") == True:
         pass
     else:
-        local_PKGLIST = False
-        print(f"{ANSI_CODES[2]}warning{ANSI_CODES[4]}: database file is not found on path so downloading it")
-        print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: creating database file")
-        if isdir("/etc/nemesis-pkg") == False:
-            mkdir("/etc/nemesis-pkg")
-        pass
-
-    print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: downloading database..")
-    try:
-        new_PKGLIST = get(pkglist_src)
-    except ConnectionError:
-        print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: file failed to download due to connectivity issue")
-        exit(1)
-    except MissingSchemasingSchema:
-        print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: file failed to download due to some exceptions")
-        exit(1)
-
-    if new_PKGLIST != None and local_PKGLIST == False:
-        print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: creating PKGLIST..")
-        local_PKGLIST = open(path_pkglist, 'w')
-        local_PKGLIST.write(str(new_PKGLIST.content.decode("utf-8")))
-        local_PKGLIST.close()
-    else:
-        local_PKGLIST = open(path_pkglist, 'r+')
-        if local_PKGLIST.read() != str(new_PKGLIST.content.decode("utf-8")):
-            print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: replacing PKGLIST with the new one")
-            local_PKGLIST.seek(0)
-            local_PKGLIST.write(str(new_PKGLIST.content.decode("utf-8")))
-            local_PKGLIST.truncate()
-            local_PKGLIST.close()
-            pass
-        else:
-            print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: you had the latest database")
-
-    print(f"{ANSI_CODES[1]}sucess{ANSI_CODES[4]}:- the databases were updated succesfully")
-
-def list_packages_from_repo(query: list[str]):
-    print(f"{ANSI_CODES[3]}info:{ANSI_CODES[4]} syncing the databases to get latest software")
-    update_database()
-    PKGLIST = open(path_pkglist , "r+")
-    PKGLIST_AVAILABLE = PKGLIST.read()
-    PKGLIST_AVAILABLE = PKGLIST_AVAILABLE.splitlines()
-    if query == []:
-        print(f"{ANSI_CODES[2]}warning{ANSI_CODES[4]}: no query specified so printing the list of available packages")
-        for i in PKGLIST_AVAILABLE:
-            print(i)
-    else:
-        for i in range(0 , len(query)):
-            pkgs_matching = []
-            qry = list(query[i])
-            for j in PKGLIST_AVAILABLE:
-                if j.find(qry[i]) == -1:
-                    continue
-                else:
-                    pkgs_matching.append(j)
-                    continue
-                    
-            if pkgs_matching == []:
-                print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: no package was relevant to {query[i]}")
-            else:
-                print(f"{ANSI_CODES[2]}note{ANSI_CODES[4]}: there were some packages matching to {query[i]}..")
-                for matching_pkgs in pkgs_matching:
-                    print(matching_pkgs)
-
-            continue
-
-def list_installed_packages(query: list[str]):
-    installed_packages = open(path_ipkglist, 'r')
-    installed_packages_content = installed_packages.read()
-    install_packages_content = installed_packages_content.splitlines()
-    if query == []:
-        for i in install_packages_content:
-            print(i)
-    else:
-        for i in range(0, len(query)):
-            print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: checking if {query[i]} is installed or not")
-            if query[i] in install_packages_content:
-                print(f"{ANSI_CODES[2]}note{ANSI_CODES[4]}: {query[i]} is installed")
-            else:
-                print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: {query[i]} is not installed")
-
-def check_if_important_files_are_there():
-    print(f"{ANSI_CODES[3]}note{ANSI_CODES[4]}: nemesis-pkg is checking that some important files are there or not...")
-    if isdir("/etc/nemesis-pkg") == True and isfile("/etc/nemesis-pkg/IPKGLIST") == True and isfile("/etc/nemesis-pkg/PKGLIST") == True:
-        print(f"{ANSI_CODES[1]}sucess{ANSI_CODES[4]}: the neccesary files are present in the system")
-    else:
-        print(f"{ANSI_CODES[2]}warning{ANSI_CODES[4]}: some files were not found so nemesis-pkg is creating it")
+        print("error: config file not found")
         
-    if isdir("/etc/nemesis-pkg") == False:
-        print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: creating /etc/nemesis-pkg")
-        mkdir("/etc/nemesis-pkg/")
-    else:
-        pass
+    chdir("/etc/nemesis-pkg")
+    path.append("/etc/nemesis-pkg")
+    import config
 
-    if isfile("/etc/nemesis-pkg/PKGLIST") == False:
-        update_database()        
+    if str(type(config.ANSI_CODES)) == "<class 'list'>" and len(config.ANSI_CODES) == 5:
+        ANSI_CODES = config.ANSI_CODES
     else:
-        pass
-        
-    if isfile("/etc/nemesis-pkg/IPKGLIST") == False:
-        print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: creating IPKGLIST")
-        file = open("/etc/nemesis-pkg/IPKGLIST" , 'w')
-        file.write('')
-        file.close()
-    else:
-        pass
+        print("config error.. ANSI_CODES is either missing or there are less than 5 numbers")
 
-def upgrade_packages():
-    print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: in order to check for the latest packages the database needs to be synced")
-    update_database()
-    read_PKGLIST = open("/etc/nemesis-pkg/PKGLIST")
-    read_IPKGLIST = open("/etc/nemesis-pkg/IPKGLIST" , 'r+')
-    content_PKGLIST = read_PKGLIST.read().splitlines()
-    content_IPKGLIST = read_IPKGLIST.read().splitlines()
-    packages_to_update = []
-    for i in range(0, len(content_PKGLIST)):
-        content_PKGLIST[i] = content_PKGLIST[i].split()
-    for i in range(0, len(content_IPKGLIST)):
-        content_IPKGLIST[i] = content_IPKGLIST[i].split()
-    for j in content_IPKGLIST:
-        for i in content_PKGLIST:
-            if j[0] in i[0] and float(j[1]) > float(i[1]):
-                packages_to_update.append(f"{i[0]} {ANSI_CODES[0]}{i[1]}{ANSI_CODES[4]} {ANSI_CODES[3]}->{ANSI_CODES[4]} {j[0]} {ANSI_CODES[1]}{j[1]}{ANSI_CODES[4]}")
-                    
-    if packages_to_update == []:
-        print(f"{ANSI_CODES[2]}note{ANSI_CODES[4]}: all packages are up to date")
+    if config.check_nessary_files == False:
+        disable_check_important_files = False
     else:
-        print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: these packages will be updated")
-        for i in packages_to_update:
-            print(i)
-
-if __name__ == "__main__":
-    if current_user != b'root\n':
-        print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: user is not root")
-        exit(1)
-    else:
-        pass
-
-    check_if_important_files_are_there()
+        disable_check_important_files = True
     
-    try:
-        if argv[1] == "h":
-            print(mainpage)
-        elif argv[1] == "v":
-            print("nemesis-pkg 0.1(Build 2368)")
-        elif argv[1] == "ud":
-            update_database()
-        elif argv[1] == "ug":
-            upgrade_packages()
-        elif argv[1] == "lri":
-            for i in range(2, len(argv)):
-                cmd_args.append(argv[i])
-            list_packages_from_repo(cmd_args)
-        elif argv[1] == "lsi":
-            for i in range(2, len(argv)):
-                cmd_args.append(argv[i])
-            list_installed_packages(cmd_args)
-        else:
-            print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: invalid operation")
-    except IndexError:
-        print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: no operation specified")
+VERSION = 0.1
+
+
