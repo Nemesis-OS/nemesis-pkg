a806f09a27f13205aabc595160d748cb48655437
Abhiraj Rik <BHK8624@dpsjaipur.com>
2023-06-16 12:21:11 +0530

    config.py
    add config
    
    default config

---
+++ config.py
@@ -0,0 +1,12 @@
+ANSI_CODES = [
+    "\x1b[31m",
+    "\x1b[32m",
+    "\x1b[33m",
+    "\x1b[34m",
+    "\x1b[0m"
+]
+
+REPOS = [
+    ["https://raw.githubusercontent.com/Nemesis-OS/packages-release/main/release.PKGLIST" , "release.PKGLIST"],
+    ["https://raw.githubusercontent.com/Nemesis-OS/packages-security/main/security.PKGLIST" , "security.PKGLIST"]
+]
