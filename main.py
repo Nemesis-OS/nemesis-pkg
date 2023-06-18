1558fc3f8dc7f2446e74a33ac1a2a0eb8b2e73c1
Abhiraj Rik <BHK8624@dpsjaipur.com>
2023-06-18 21:34:24 +0530

    main.py
    user can delete snapshot and ctrlc detection

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from os import mkdir, chdir, getlogin
+from os import mkdir, chdir, getlogin, system
 from os.path import isdir, isfile
 from sys import argv, exit, path
 from subprocess import check_output
+from shutil import copy
 from time import strftime
 try:
     from requests import get
@@ -11,7 +12,7 @@ except ImportError:
     print("error: requests not found..")
     exit(1)
 
-disable_check_important_files = True
+#disable_check_important_files = True
 ANSI_CODES = []
 REPOLIST = []
 
@@ -75,6 +76,8 @@ def sync_packages(PKGLIST: list[list[str]]):
         ctime = strftime("%D %H:%M:%S")
         write_history(f"{ctime} {ANSI_CODES[2]}{PKGLIST[i][2]}{ANSI_CODES[4]} repo updated..")
 
+    write_hist_snapshot()
+
 def write_history(current_opr: str):
     history_npkg = False
     if isfile(f"/home/{getlogin()}/.nemesis-pkg_history") == True:
@@ -94,22 +97,120 @@ def write_history(current_opr: str):
         history.truncate()
         history.close()
 
+    copy(f"/home/{getlogin()}/.nemesis-pkg_history" , "/etc/nemesis-pkg/.nemesis-pkg_history")
+
+def delete_history(date: str):
+    try:
+        hist_file = open(f"/home/{getlogin()}/.nemesis-pkg_history" , 'r+')
+    except PermissionError:
+        print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: you need to be root in order to run this")
+        exit(1)
+
+    print(date)
+    if date == "all":
+        hist_file.seek(0)
+        hist_file.truncate()
+        hist_file.close()
+    else:
+        a = []
+        history = hist_file.read()
+        print(f"{ANSI_CODES[3]}note{ANSI_CODES[4]}: deleting {ANSI_CODES[2]}{date}{ANSI_CODES[4]} from history")
+        for i in range(0,len(history.splitlines())):
+            if history.splitlines()[i].find(date) > -1:
+                print(f"{ANSI_CODES[3]}note{ANSI_CODES[4]}: {ANSI_CODES[2]}{date}{ANSI_CODES[4]} found so deleting it")
+            else:
+                a.append(history.splitlines()[i])
+                continue
+
+        history = ''
+        for i in range(0 , len(a)):
+            history = history + a[i]
+            
+        hist_file.seek(0)
+        hist_file.write(history)
+        hist_file.truncate()
+        hist_file.close()
+
+def write_hist_snapshot():
+    snapshot_exists = True
+    print(f"{ANSI_CODES[3]}note{ANSI_CODES[4]}: writing snapshot of the history")
+    try:
+        chdir("/etc/nemesis-pkg/.history_snapshots")
+    except FileNotFoundError:
+        snapshot_exists = False
+        print(f"{ANSI_CODES[2]}warning{ANSI_CODES[4]}: history snapshots directory not found so creating it..")
+        chdir("/etc/nemesis-pkg")
+        mkdir("/etc/nemesis-pkg/.history_snapshots")
+        pass
+
+    if snapshot_exists == False:
+        chdir("/etc/nemesis-pkg/.history_snapshots")
+        file_metadata = open("meta" , 'w')
+        metanum = 0
+        file_metadata.write(str(metanum))
+        file_metadata.close()
+    else:
+        if isfile("meta") == True:
+            file_metadata = open("meta" , 'r+')
+            metanum = file_metadata.read()
+            metanum = int(metanum)+1
+            file_metadata.seek(0)
+            file_metadata.write(str(metanum))
+            file_metadata.truncate()
+            file_metadata.close()
+        #else:
+        
+
+    time = strftime("%d_%m_%y")
+    print(f"{ANSI_CODES[3]}note{ANSI_CODES[4]}: copying history to the snapshots folder")
+    copy(f"/etc/nemesis-pkg/.nemesis-pkg_history" , f"{str(metanum)}_{time}_history.bak")
+    print(f"{ANSI_CODES[3]}note{ANSI_CODES[4]}: snapshot of history was taken succesfully")
+
+def delete_snapshot(id: int):
+    if id == 0:
+        if input(f"{ANSI_CODES[2]}warning{ANSI_CODES[4]}: this is an irreversible operation and all the saved snapshots will be lost.. do you want to continue it[{ANSI_CODES[1]}y{ANSI_CODES[4]}/{ANSI_CODES[0]}N{ANSI_CODES[4]}] ") == "y":
+            chdir("/etc/nemesis-pkg/.history_snapshots")
+            if system("rm -rf *.bak") != 0:
+                print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: snapshot was not deleted due to some errors")
+            else:
+                print(f"{ANSI_CODES[1]}sucess{ANSI_CODES[4]}: snapshot deleted sucessfully")
+        else:
+            print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: user decided to cancel")
+    
 VERSION = 0.1
-BUILD_NUM = 23617
+BUILD_NUM = 23618
 
 if __name__ == "__main__":
     parse_config_file()
     try:
-        if len(argv) > 1 and argv[1] == "update":
+        if len(argv) >= 2 and argv[1] == "update":
             sync_packages(REPOLIST)
-        elif len(argv) > 1 and argv[1] == "version" or argv[1] == "-v":
+        elif len(argv) >= 2 and argv[1] == "version" or argv[1] == "-v":
             print(f"nemesis-pkg build {VERSION} {BUILD_NUM}")
-        elif len(argv) > 1 and argv[1] == "history":
-            file = open(f"/home/{getlogin()}/.nemesis-pkg_history")
-            print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: this is the complete history of operations run by nemesis-pkg")
-            print(file.read())
-            file.close()
+        elif len(argv) > 2 and argv[1] == "history":
+            if argv[2] == "view":
+                file = open(f"/home/{getlogin()}/.nemesis-pkg_history")
+                print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: this is the complete history of operations run by nemesis-pkg")
+                print(file.read())
+                file.close()
+            elif len(argv) == 4 and argv[2] == "delete":
+                delete_history(argv[3])
+            elif len(argv) > 4 and argv[2] == "snapshot":
+                if argv[4] == "all":
+                    delete_snapshot(0)
+                else:
+                    delete_snapshot(int(argv[4]))
+            elif len(argv) == 3 and argv[2] == "help":
+                print("nemesis-pkg history help\n========================\nview: views the history file to see operations run by users\ndelete: this command deletes history from a given date. all keyword clears history\nsnapshot: this command allows to view/undo/redo history snapshots")
+        elif len(argv) == 2 and argv[1] == "history":
+                file = open(f"/home/{getlogin()}/.nemesis-pkg_history")
+                print(f"{ANSI_CODES[3]}info{ANSI_CODES[4]}: this is the complete history of operations run by nemesis-pkg")
+                print(file.read())
+                file.close()
         else:
             print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: invalid operation")
     except IndexError:
         print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: no operation specified")
+    except KeyboardInterrupt:
+        print(f"{ANSI_CODES[0]}error{ANSI_CODES[4]}: user pressed CTRL+C so exiting")
+        exit(1)
