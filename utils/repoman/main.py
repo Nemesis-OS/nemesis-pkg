1917688513fe2a2a6bde18d50128d88327e73d4c
Abhiraj Rik <133798548+abrik1@users.noreply.github.com>
2023-08-04 22:53:00 +0530

    add help page and view page

--- utils/repoman/main.py
+++ utils/repoman/main.py
@@ -6,11 +6,17 @@ import sys
 from os import getuid
 from os.path import isfile
 from subprocess import check_output
+from json import loads
+
 
 t_col = ["\x1b[31m",
          "\x1b[32m",
          "\x1b[34m",
          "\x1b[0m"]
+
+vrepos = []
+repos = []
+repo_count = 1
 
 def sync_verified_repos():
     '''
@@ -34,9 +40,20 @@ def sync_verified_repos():
         repo_db.close()
 
 def parse_db():
+    global vrepos, repos
     '''
     this function parses database...
     '''
+    with open('/etc/nemesis-pkg/verified_repos.json', 'r', encoding="utf-8") as vrdb:
+        vrepos = loads(vrdb.read())
+
+    repos = list(vrepos.keys())
+
+def view_info():
+    for i in repos:
+        print("{}{}{}: {}{}{}".format(t_col[1], repo_count, t_col[3], t_col[2], vrepos[i]['ID'], t_col[3]))
+        print("\t{}Description{}: {}".format(t_col[2], t_col[3], vrepos[i]['DESC']))
+        print("\t{}Source{}: {}".format(t_col[2], t_col[3], vrepos[i]['COMMAND_TO_ADD'][3]))
         
 if __name__ == "__main__":
     if getuid() == 0:
@@ -44,5 +61,15 @@ if __name__ == "__main__":
     else:
         print("{}error{}: please run this as root".format(t_col[0], t_col[3]))
         sys.exit(1)
-        
+
     sync_verified_repos()
+    parse_db()
+
+    if sys.argv[1] == "v" or sys.argv[1] == "view":
+        view_info()
+    else:
+        print('''nemesis-pkg-rpoman: a tool to manage repos
+==========================================
+[a]dd: add repository
+[h]elp: show this page
+[v]iew: view verified repos''')
