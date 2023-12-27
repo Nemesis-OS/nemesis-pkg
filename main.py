f05cd0266027cedc8ae63fefe92fabcf466862be
Abhiraj Rik <sontaimnt@gmail.com>
2023-12-27 12:01:50 +0530

    list works.. partial search and some fixes

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def use_config():
         except KeyError:
             print("=> CONFIG ERROR:")
     else:
-        print("=> \x1b[33;1mWARNING\x1b[0m: using fallback config")
+        print("=> \x1b[33;1mwarning:\x1b[0m using fallback config")
 
 def update_database():
     '''
@@ -69,7 +69,7 @@ def update_database():
     # use repositories locally
     for i in repos:
         name = get_fname(get_file_from_url(i))
-        print(f"=> \x1b[34;1mINFO:\x1b[0m downloading database for repo \x1b[33;1m{name}\x1b[0m")
+        print(f"=> \x1b[34;1minfo:\x1b[0m downloading database for repo \x1b[33;1m{name}\x1b[0m")
         try:
             contents = get(i).content.decode('utf-8')
 
@@ -79,37 +79,61 @@ def update_database():
             with open(f"/etc/nemesis-pkg/{name}.PKGLIST", 'w') as db:
                 db.write(contents)
             db.close()
-            print(f"=> \x1b[32;1mSUCESS:\x1b[0m database fore repo \x1b[33;1m{name}\x1b[0m is upto date")
+            print(f"=> \x1b[32;1msucess:\x1b[0m database fore repo \x1b[33;1m{name}\x1b[0m is upto date")
 
         except (MissingSchema, ConnectTimeout, ConnectionAbortedError, ConnectionRefusedError, ConnectionResetError, ConnectionError):
-            print(f"=> \x1b[31;1mERROR\x1b[0m: downloading database for repo \x1b[33;1m{name}\x1b[0m failed")
+            print(f"=> \x1b[31;1merror:\x1b[0m downloading database for repo \x1b[33;1m{name}\x1b[0m failed")
             return 1
 
+def list_packages():
+    '''
+    list-
+    '''
+        
 if __name__ == "__main__":
     try:
         if len(argv) == 2:
             match argv[1]:
                 case "-h" | "--help" | "help" | "h" | "[h]elp":
-                    print('''nemesis-pkg help page\x1b[0m:
-======================
-[\x1b[31;1mh\x1b[0m]\x1b[1melp\x1b[0m - show this page
-[\x1b[32;1mi\x1b[0m]\x1b[1mnstall\x1b[0m - install \x1b[33m<pkg>\x1b[0m
-[\x1b[33;1ml\x1b[0m]\x1b[1mist\x1b[0m - list packages
-[\x1b[34;1ms\x1b[0m]\x1b[1mearch\x1b[0m - search \x1b[33m<pkg>\x1b[0m from database
-[\x1b[35;1mu\x1b[0m]\x1b[1mpdate\x1b[0m - update package database
-[\x1b[36;1mU\x1b[0m]\x1b[1mpgrade\x1b[0m - upgrade installed packages
-[\x1b[31;1mv\x1b[0m]\x1b[1mersion\x1b[0m - show nemesis-pkg version''')
+                    print('''nemesis-pkg: software package manager
+ACTIONS nemesis-pkg {h|i|l|s|u|U|v} [...]
+                    
+  [\x1b[31;1mh\x1b[0m]\x1b[1melp\x1b[0m\tshow this help message
+  [\x1b[31;1mi\x1b[0m]\x1b[1mnstall\x1b[0m\tinstall the given package
+  [\x1b[31;1ml\x1b[0m]\x1b[1mist\x1b[0m\tlist all installed packages
+  [\x1b[31;1ms\x1b[0m]\x1b[1mearch\x1b[0m\tsearch for the given package
+  [\x1b[31;1mu\x1b[0m]\x1b[1mpdate\x1b[0m\tupdate the package database
+  [\x1b[31;1mU\x1b[0m]\x1b[1mpgrade\x1b[0m\tupgrade all installed packages
+  [\x1b[31;1mv\x1b[0m]\x1b[1mersion\x1b[0m\tprint the program version
+                    ''')
                 case "-v" | "--version" | "version" | "v" | "[v]ersion":
                     print("nemesis-pkg 0.1")
                 case "-u" | "update" | "--update" | "u" | "[u]pdate":
                     if is_root():
                         update_database()
                     else:
-                        print("=> \x1b[31;1mERROR:\x1b[0m \x1b[33;1msync\x1b[0m needs to be run as superuser")
+                        print("=> \x1b[31;1merror:\x1b[0m \x1b[33;1msync\x1b[0m needs to be run as superuser")
+                case "-s" | "search" | "--search" | "s" | "[s]earch":
+                    if len(argv) < 3:
+                        print("=> \x1b[34;1musage:\x1b[0m nemesis-pkg search package")
+                        print("=> \x1b[31;1merror:\x1b[0m expected \x1b[33;1m1\x1b[0m argument but got \x1b[33;1m0\x1b[0m")
+                        exit(1)
+
+                    search_package(argv[2])
+                case "l" | "list" | "--list" | "-l" | "[l]ist":
+                    for i in repos:
+                        try:
+                            with open(f"/etc/nemesis-pkg/{get_file_from_url(i)}") as db:
+                                for j in db.read().splitlines():
+                                    print(j)
+                            db.close()
+                        except FileNotFoundError:
+                            continue
+                    
                 case _:
-                    print("=> \x1b[31;1mERROR:\x1b[0m invalid choice")
+                    print("=> \x1b[31;1merror:\x1b[0m invalid choice")
         else:
-            print("=> \x1b[31;1mERROR:\x1b[0m nemesis-pkg recieved no arguments")
+            print("=> \x1b[31;1merror:\x1b[0m nemesis-pkg recieved no arguments")
     except KeyboardInterrupt:
-        print("=> \x1b[31;1mERROR:\x1b[0m \x1b[33;1mControl-C\x1b[0m pressed so exiting")
+        print("=> \x1b[31;1merror:\x1b[0m \x1b[33;1mControl-C\x1b[0m pressed so exiting")
         exit(1)
