bfe01e1d29d922fd79b3a1df2ba40c873eaa3c82
abrik1 <sontaimnt@gmail.com>
2024-12-22 14:26:00 +0530

    npkg-query: implement help subcommand

--- npkg-query.cr
+++ npkg-query.cr
@@ -18,6 +18,17 @@ begin
     if ARGV[0] == "version" || ARGV[0] == "v"
         puts "=> npkg-query #{version}"
         puts "=> built by Crystal #{Crystal::VERSION} for #{Crystal::HOST_TRIPLE}"
+    elsif ARGV[0] == "help" || ARGV[0] == "h"
+        puts "=> npkg-query [#{"h,l,s,v".colorize(:magenta)}] {..}"
+        puts "subcommands:".colorize.underline
+        puts "  {#{"h".colorize(:light_magenta)}}#{"elp".colorize.bold}\t show this page"
+        puts "  {#{"l".colorize(:light_magenta)}}#{"ist".colorize.bold}\t list installed pkgs"
+        puts "  {#{"s".colorize(:light_magenta)}}#{"each".colorize.bold}\t search the specified pkg"
+        puts "  {#{"v".colorize(:light_magenta)}}#{"ersion".colorize.bold}\t show the current version"
+        puts "examples:".colorize.underline
+        puts "=> #{"note".colorize(:light_magenta)}: subcommands in #{"yellow".colorize(:light_yellow)}, args in #{"red".colorize(:light_red)} and comments in #{"blue".colorize(:light_blue)}"
+        puts "  $ npkg-query #{"search".colorize(:light_yellow)} #{"firefox".colorize(:light_red)} #{"# searching pkgs similar to firefox".colorize(:light_blue)}"
+        puts "  $ npkg-query #{"list".colorize(:light_yellow)} #{"# finding which packages are installed".colorize(:light_blue)}"
     else
         unless ARGV[0] == nil
             puts "=> #{"error:".colorize(:light_red)} available args are #{["help", "list", "search", "version"].colorize(:magenta)}"
