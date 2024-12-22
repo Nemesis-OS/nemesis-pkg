accf6e16f5c0b461fe8021935902603810b9b248
abrik1 <sontaimnt@gmail.com>
2024-12-22 10:30:47 +0530

    npkg-query: add version subcommand

--- npkg-query.cr
+++ npkg-query.cr
@@ -1,5 +1,10 @@
 # npkg-query: package searcher for nemesis-pkg
 
+# TODO:
+# - [ ] list packages installed
+# - [ ] search packages
+# - [ ] in search show what pkgs are out of date
+
 version = 0.1
 
 require "colorize"
@@ -12,5 +17,16 @@ Colorize.enabled = Colorize.on_tty_only!()
 # load files, first we check if they exist
 unless File.exists?("/etc/nemesis-pkg/config.json") && File.exists?("/etc/nemesis-pkg/pkgdata.json") 
     puts "=> #{"error:".colorize(:red)} neccesary files not found."
+    Process.exit(1)
 end
- 
+
+# arguement parser
+begin
+    if ARGV[0] == "version" || ARGV[0] == "v"
+        puts "=> npkg-query #{version}"
+        puts "=> built by Crystal #{Crystal::VERSION} for #{Crystal::HOST_TRIPLE}"
+    end
+rescue
+    puts "=> #{"error:".colorize(:red)} some error occured."
+    Process.exit(1)
+end
